aa590b280e051b4e10871a3e4ceca72516470dab
gary.abramson@gmail.com <gary.abramson@gmail.com>
2024-01-10 18:50:59 -0600

    Initial commit

---
+++ HoopsService.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HoopsTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ HoopsService/src/tournament/team.cs
@@ -0,0 +1,19 @@
+﻿public class Team
+{
+    public int Id { get; }
+    public string Name { get; }
+    public int Seed { get; }
+
+    public Team(int id, string name, int seed)
+    {
+        if (id < 1 || id > 64)
+            throw new ArgumentOutOfRangeException(nameof(id), "Id must be between 1 and 64.");
+
+        if (seed < 1 || seed > 16)
+            throw new ArgumentOutOfRangeException(nameof(seed), "Seed must be between 1 and 16.");
+
+        Id = id;
+        Name = name;
+        Seed = seed;
+    }
+}
